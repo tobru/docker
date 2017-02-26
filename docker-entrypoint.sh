234c4d8bc48570143a0d8250385b018d7ca67479
Tobias Brunner <tobias@tobru.ch>
2017-02-26 18:06:27 +0100

    improve entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,8 +2,9 @@
 set -e
 
 if [ ! -e '/var/www/html/version.php' ]; then
-    tar cf - --one-file-system -C /usr/src/nextcloud . | tar xf -
-    #chown -R www-data /var/www/html
+  echo "Extracting app source to /var/www/html"
+  tar cf - --one-file-system -C /usr/src/nextcloud . | tar xf - --no-overwrite-dir
+  #chown -R www-data /var/www/html
 fi
 
 exec "$@"
